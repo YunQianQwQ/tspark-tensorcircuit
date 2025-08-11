eecae4de1cb10e7696615f61c704b0740a4f6344
liuhengxi <2019891597@qq.com>
2025-08-11 21:09:12 +0800

    compiler

--- tensorcircuit/test2.cpp
+++ tensorcircuit/test2.cpp
@@ -1,6 +1,3 @@
-#include <pybind11/pybind11.h>
-#include <string>
-
 #include<iostream>
 #include<sstream>
 #include<algorithm>
@@ -336,16 +333,17 @@ void process1()
 	oss<<"\n";
 }
 
-namespace py = pybind11;
-
 static std::string process(const std::string& s) {
     iss<<s;
+    process1();
     string t;
     oss>>t;
     return t;
 }
 
-PYBIND11_MODULE(mylib, m) {
-    m.doc() = "pybind11 single-file example";
-    m.def("process", &process, py::arg("s"));
+int main()
+{
+	static char s[1<<21];
+	cin.read(s,1<<20);
+	cout<<process(string(s));
 }
